ddd4d2a45486a816f5d32c804cd81681e561e761
zahr2-lab <69382928+zahr2-lab@users.noreply.github.com>
2020-11-16 22:41:29 +0330

    update

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,22 @@
-import styles from "./styles.module.scss";
-
 export default function IndexPage() {
   return (
-    <div className={styles.body}>
-      <div className={styles.topBar}>Beirut Food</div>
-      <img className={styles.img} src="/img/menu.png" alt="" />{" "}
+    <div className="container">
+      <div className="topBar">Beirut Food</div>
+      <img className="img" src="/img/menu.png" alt="" />{" "}
+      <style jsx>{`
+        .container {
+          margin: 0;
+        }
+        .topBar {
+          background: red;
+          color: white;
+          padding: 0.5rem;
+          font-size: 1.5rem;
+        }
+        .img {
+          width: calc(100vw - 1rem);
+        }
+      `}</style>
     </div>
   );
 }
