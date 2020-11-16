549aa5d44e7a0b38d7657a42ed06962e1ada8901
zahr2-lab <69382928+zahr2-lab@users.noreply.github.com>
2020-11-16 21:44:48 +0330

    b

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 export default function IndexPage() {
   return (
     <div>
-      <div>Farshid Marketing</div>
+      <div>Beirut Marketing</div>
       <img width="300px" src="/img/marketing.png" alt="" />{" "}
     </div>
   );
